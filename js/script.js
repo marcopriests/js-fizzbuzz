debfbd1ddc9f5aed2129fa96d934817ab6c193ee
marcopriests <prtmrc98@gmail.com>
2025-03-18 14:41:30 +0100

    [UPD] Buzz done

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ for (let counter = 1; counter <= 100; counter++) {
     
     if (counter % 3 == 0) {
         console.log("Fizz")
+    } else if (counter % 5 == 0) {
+        console.log("Buzz")
     } else {
 
         console.log(counter)
