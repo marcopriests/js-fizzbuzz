073b50af7c26b1350060f70a4f3f42a92113a44a
marcopriests <prtmrc98@gmail.com>
2025-03-18 14:42:56 +0100

    [DONE] esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 for (let counter = 1; counter <= 100; counter++) {
     
-    if (counter % 3 == 0) {
+    if (counter % 3 == 0 && counter % 5 ==0) {
+        console.log("FizzBuzz")
+    } else if (counter % 3 == 0) {
         console.log("Fizz")
     } else if (counter % 5 == 0) {
         console.log("Buzz")
